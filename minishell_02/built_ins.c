4392054be944b562c87ef0efc1418f06a2cc6e6c
aben-dhi <bdamoura8@gmail.com>
2023-10-03 00:03:44 +0100

    cd function added in 02/10

--- minishell_02/built_ins.c
+++ minishell_02/built_ins.c
@@ -6,7 +6,7 @@
 /*   By: aben-dhi <aben-dhi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 20:02:55 by htouil            #+#    #+#             */
-/*   Updated: 2023/10/02 20:04:29 by aben-dhi         ###   ########.fr       */
+/*   Updated: 2023/10/02 23:56:10 by aben-dhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -148,6 +148,64 @@ void	ft_pwd(t_env *env)
 	}
 }
 
+t_env	*ft_update_pwd(t_env *env, char *old)
+{
+	t_env	*e_tmp;
+
+	e_tmp = env;
+	while (e_tmp)
+	{
+		if (ft_strcmp(e_tmp->envar, "OLDPWD") == 0)
+		{
+			// free(e_tmp->enval);
+			e_tmp->enval = ft_strcpy(old);
+		}
+		if (ft_strcmp(e_tmp->envar, "PWD") == 0)
+		{
+			// free(e_tmp->enval);
+			e_tmp->enval = malloc(2000 * sizeof(char));
+			e_tmp->enval = getcwd(e_tmp->enval, 2000);
+		}
+		e_tmp = e_tmp->next;
+	}
+	return (env);
+}
+
+t_env	*ft_cd(t_token *token, t_env *env)
+{
+	// t_token	*t_tmp;
+	t_env	*e_tmp;
+	char	*old;
+
+	old = malloc(2000 * sizeof(char));
+	if (!old)
+		exit (1);
+	old = getcwd(old, 2000);
+	if (token->next == NULL)
+	{
+		e_tmp = env;
+		while (e_tmp)
+		{
+			if (ft_strcmp(e_tmp->envar, "HOME") == 0)
+			{
+				if (chdir(e_tmp->enval) == -1)
+					perror("chdir");
+				env = ft_update_pwd(env, old);
+				break ;
+			}
+			e_tmp = e_tmp->next;
+		}
+		free (old);
+	}
+	else
+	{
+		if (chdir(token->next->value) == -1)
+			perror("chdir");
+		env = ft_update_pwd(env, old);
+	}
+	return (env);
+}
+
 void	execute_builtins(t_token *token, t_env *env)
 {
 	if (!token)
@@ -160,4 +218,6 @@ void	execute_builtins(t_token *token, t_env *env)
 		env = ft_unset(token, env);
 	else if (ft_strcmp(token->value, "pwd") == 0)
 		ft_pwd(env);
+	else if (ft_strcmp(token->value, "cd") == 0)
+		env = ft_cd(token, env);
 }
